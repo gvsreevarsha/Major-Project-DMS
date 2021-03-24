796e92dfcee0a0288d34588ceae70ef563792638
varsha <gvsreevarsha@gmail.com>
2021-03-24 18:12:48 +0530

    Ashok work Manage permitte

---
+++ AfterLogin/Manage permitte.php
@@ -0,0 +1,32 @@
+<?php require 'LoginHeader.php'?>
+<div class="card container-fluid">
+  <div style="background-color: white;" class="row p-2">
+    <div class="col-sm-12">&nbsp;</div>
+    <div>TO DELETE OR TRANSFER, CLICK ON PERMITEE</div>
+    <br/>
+    Showing 1 to 68 of 68 entries<button>Send to Excel</button><br/>
+    <table>
+      <thead>
+        <tr>
+          <th>Sno</th>
+          <th>Sno</th>
+          <th>Sno</th>
+          <th>Sno</th>
+          <th>Sno</th>
+          <th>Sno</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  </div> 
+</div>
+<div class="container-fluid mt-3">
+  <div class="row">
+    
+  </div>
+</div>
+<?php require 'LoginFooter.php'?>
