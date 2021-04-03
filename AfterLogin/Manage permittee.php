59d6a317cb65337036a1cf29ddc7575af45fd985
Ashok K.R <ashokgowda9945@gmail.com>
2021-04-03 17:40:57 +0530

    a1

---
+++ AfterLogin/Manage permittee.php
@@ -0,0 +1,64 @@
+<?php require 'LoginHeader.php'?>
+<div class="card container-fluid">
+  <div style="background-color: white;" class="row p-2">
+    <div class="col-sm-12">&nbsp;</div>
+    <div>TO DELETE OR TRANSFER, CLICK ON PERMITEE</div>
+    <br/>
+    showing 1 to 68 of 68 entries      <button> Send to Excel</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Sno</th>
+          <th>Permittee Name</th>
+          <th>Agency</th>
+          <th>Existing SV</th>
+          <th>Added On</th>
+          <th>Added By</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>KUSHIK A</td>
+          <td>MACHANICAL</td>
+          <td>1</td>
+          <td>31-2-2021 16:2:16</td>
+          <td>P SRINIVAS</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>SHASRTY CH VSR</td>
+          <td>CIVIL</td>
+          <td>0</td>
+          <td>31-2-2021 16:02:59</td>
+          <td>P SRINIVAS</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>VENKATESHWARLU A</td>
+          <td>SAFETY</td>
+          <td>0</td>
+          <td>31-2-2021 16:2:36</td>
+          <td>P SRINIVAS</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>LAHARI</td>
+          <td>TECHINICAL SERVICES</td>
+          <td>0</td>
+          <td>1-03-2021 04:49:18</td>
+          <td>ANIL</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>KISHLAY KUMAR</td>
+          <td>INSTRUMENTATION</td>
+          <td>4</td>
+          <td>18-06-2021 16:28:08</td>
+          <td>YUVARAJ</td>
+        </tr>
+</div>
+<div class="container-fluid mt-3">
+  <div class="row">
+    
+  </div>
+</div>
+<?php require 'LoginFooter.php'?>
